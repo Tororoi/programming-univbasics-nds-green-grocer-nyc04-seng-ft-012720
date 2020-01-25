407f7ccc942cd1e3401678c936080ad802d41625
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-25 20:23:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,10 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0
+  while i < collection.length do
+    name == collection[i][:item] ? return collection[i] : i += 1
+  end
 end
 
 def consolidate_cart(cart)
