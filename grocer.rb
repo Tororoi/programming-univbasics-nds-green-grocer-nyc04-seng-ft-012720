ada41bd45da05fcf654bdc53dca2f3a2367b6673
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-25 20:39:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,16 +35,53 @@ def consolidate_cart(cart)
   result
 end
 
+def mk_coupon_hash(c)
+  rounded_unit_price = (c[:cost].to_f * 1.0 / c[:num]).round(2)
+  {
+    :item => "#{c[:item]} W/COUPON",
+    :price => rounded_unit_price,
+    :count => c[:num]
+  }
+end
+
+def apply_coupon_to_cart(matching_item, coupon, cart)
+  matching_item[:count] -= coupon[:num]
+  item_with_coupon = mk_coupon_hash(coupon)
+  item_with_coupon[:clearance] = matching_item[:clearance]
+  cart << item_with_coupon
+end
+
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0
+  while i < coupons.count do
+    coupon = coupons[i]
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item],cart)
+    item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+
+    if item_is_in_basket and count_is_big_enough_to_apply
+      apply_coupon_to_cart(item_with_coupon,coupon,cart)
+    end
+    i += 1
+  end
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  i = 0
+  while i < cart.length do
+    item = cart[i]
+    if item[:clearance]
+      discounted_price = ((1 - CLEARANCE_ITEM_DISCOUNT_RATE) * item[:price]).round(2)
+        item[:price] = discounted_price
+    end
+    i += 1
+  end
 end
 
 def checkout(cart, coupons)
